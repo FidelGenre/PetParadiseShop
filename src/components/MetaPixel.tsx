b4e0ddca8231350046c7be73a52f9523103d93a0
perezlindoignacio8-hash <perezlindoignacio8@gmail.com>
2026-05-22 22:16:20 -0300

    .

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, Suspense } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 declare global {
@@ -10,17 +10,19 @@ declare global {
   }
 }
 
-export default function MetaPixel() {
+function MetaPixelTracker() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    // Track PageView on every route change
     window.fbq?.('track', 'PageView');
   }, [pathname, searchParams]);
 
+  return null;
+}
+
+export default function MetaPixel() {
   useEffect(() => {
-    // Initialize Meta Pixel
     const script = document.createElement('script');
     script.innerHTML = `
       !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
@@ -30,7 +32,6 @@ export default function MetaPixel() {
     script.async = true;
     document.head.appendChild(script);
 
-    // Add noscript image
     const noscript = document.createElement('noscript');
     const img = document.createElement('img');
     img.height = 1;
@@ -41,5 +42,9 @@ export default function MetaPixel() {
     document.head.appendChild(noscript);
   }, []);
 
-  return null;
+  return (
+    <Suspense fallback={null}>
+      <MetaPixelTracker />
+    </Suspense>
+  );
 }
